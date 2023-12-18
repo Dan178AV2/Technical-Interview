2703b8deb5dc59dfbab152dbdd6a0496024480c6
Dan A <139174165+Dan178AV2@users.noreply.github.com>
2023-12-18 15:25:49 -0400

    Update 1_prueba.py

--- 1_prueba.py
+++ 1_prueba.py
@@ -1,36 +1,19 @@
 
 import matplotlib.pyplot as plt
 
-# Carga los datos desde el archivo JSON
+# 1) Carga los datos desde el archivo JSON
 
-# Ordena los datos por el valor de 'x' en orden descendente (Prueba)
+# 2) Ordena los datos por el valor de 'x' en orden descendente
 
-# Define el diccionario MODULES is opacity in color blue
-MODULES = {
-   0: '0%',
-   1: '10%',
-   2: '20%',
-   3: '30%',
-   4: '40%',
-   5: '50%',
-   6: '60%',
-   7: '70%',
-   8: '80%',
-   9: '90%',
-   10: '100%'
-}
+# 3) Asigna un valor de opacidad ( 0 a 100% ) a cada valor de z obtenidos en el paso 2
 
-# Structure: type json = {
-#   x: number;
-#   y: number;
-#   z: number;
-#   type: string;
-# }
 
-# Define una función filterd_z(Prueba) que filtra los datos basándose en el valor de 'z' y
-# agrega un nuevo campo 'type' a cada diccionario que pasa el filtro
-def filterd_z(filt):
+# 4) Filtra los datos por el valor de 'z' y agrega un nuevo valor llamado 'tipo' usando la funcion filterd_z
+
+
+def filterd_z(filt: str) -> list:
     """
+    Consejo de la función (implementala como desees):
     La función filtra una lista de diccionarios en función de un valor específico para la clave 'z' y
     agrega un nuevo 'tipo' de clave con un valor correspondiente de un diccionario predefinido.
 
@@ -38,25 +21,26 @@ def filterd_z(filt):
     desea filtrar los datos
     :return: una matriz JSON que contiene elementos de la matriz "datos" donde el valor de la clave "z"
     coincide con el filtro proporcionado. Cada elemento de la matriz JSON devuelta también incluye un
-    nuevo "tipo" de par clave-valor al que se le asigna el valor del diccionario MÓDULOS en función del
+    nuevo "tipo" de par clave-valor al que se le asigna el valor de la opacidad en función del
     valor "z".
     """
     pass
 
-  # Itera sobre los elementos de MODULES
+
+# 5) Completa el siguiente for
 for idx in range(1, 11):
     filtered_data = filterd_z(idx)
-    # Convierte los valores de 'type' a números decimales
-    opacities = list()
     # Crea los arrays de valores para el eje x y el color
     x = []
     # Asumiendo que 'y' es otra columna en tus datos
     y = []
-    color = []  # Azul con diferentes opacidades
+    color = []  # opacidades
+
     # Grafica los datos
     for xi, yi, ci in zip(x, y, color):
         plt.scatter(xi, yi, color=ci)
 plt.show()
-# En resumen, este código genera un conjunto de datos aleatorios,
-# los guarda en un archivo JSON, los lee y los manipula de varias maneras,
-# y finalmente visualiza los datos en un gráfico de dispersión.
+
+# En resumen, este codigo debe graficar los datos en un scatter
+# pero si deseas hacerlo en tu propia logica puedes hacerlo
+
